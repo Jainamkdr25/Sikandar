44102464a535fc8117a92cc2e5190adc8fd164c7
Jainamkdr25 <Legacyroxz123@outlook.com>
2024-12-19 00:43:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7527768602:AAGmoQZQ6G9aLNSnETE0gtos3bZUDJcV0aQ")
+BOT_TOKEN=("7511149271:AAElDMPHnywI3Y44mIVfo7sA_kxZ-TkUNAE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@H3X_neeraj")
+OWNER_USERNAME=("@God_x_Sikandar")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1216702307, 5004674194")
+ADMIN_IDS=("1216702307")
 
 
 
